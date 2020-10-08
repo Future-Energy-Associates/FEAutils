96b34c0ff548d7bb4cde02ca86e3481c8b958463
Ayrton Bourn <ayrtonbourn@outlook.com>
2020-10-08 11:46:21 +0100

    added R-like summarise functionality

--- FEAutils/general.py
+++ FEAutils/general.py
@@ -2,7 +2,28 @@ import pandas as pd
 import requests
 from IPython.display import JSON
 
-# general functions
+# General functions
+from collections.abc import Iterable
+
+cond_attr = lambda df, attr, cond='tuple()', col=None: getattr(df.query(cond), attr)() if col is None else getattr(df.query(cond)[col], attr)()
+cond_func = lambda df, func, cond='tuple()', col=None: func(df.query(cond)) if col is None else func(df.query(cond)[col])
+
+def summarise(df, agg_funcs):
+    cleaned_agg_funcs = {
+        k: (v if isinstance(v, Iterable) else (v, {}))
+        for k, v 
+        in agg_funcs.items()
+    }
+    
+    df_summarised = (df
+                     .apply(lambda df: pd.Series({
+                         name: func(df, **kwargs) 
+                         for name, (func, kwargs)
+                         in cleaned_agg_funcs.items()
+                     }))
+                    )
+    
+    return df_summarised
 
 reverse_dict = lambda original_dict: {
     value: key for key, value in original_dict.items()
@@ -16,6 +37,8 @@ keep_index = lambda df, func, *args, **kwargs: pd.DataFrame(
 
 view_dict_head = lambda d, n=5: JSON(pd.Series(d).head(n).to_dict())
 
+
+# Slack
 msg_2_data = lambda record_msg: f'{"{"}"text":"{record_msg}"{"}"}'
 
 
