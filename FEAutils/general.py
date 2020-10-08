24e0e26fdc0c5f0964dae7d63a9705e2c85cb933
Ayrton Bourn <ayrtonbourn@outlook.com>
2020-10-08 15:07:00 +0100

    improved summarisation functionality and docs

--- FEAutils/general.py
+++ FEAutils/general.py
@@ -5,25 +5,104 @@ from IPython.display import JSON
 # General functions
 from collections.abc import Iterable
 
-cond_attr = lambda df, attr, cond='tuple()', col=None: getattr(df.query(cond), attr)() if col is None else getattr(df.query(cond)[col], attr)()
-cond_func = lambda df, func, cond='tuple()', col=None: func(df.query(cond)) if col is None else func(df.query(cond)[col])
+def summarise_wrapper(df, operation, cond='tuple()', col=None, wrapper_type='auto'):
+    """
+    Carries out the individual summary operations 
 
-def summarise(df, agg_funcs):
-    cleaned_agg_funcs = {
-        k: (v if isinstance(v, Iterable) else (v, {}))
-        for k, v 
-        in agg_funcs.items()
+    Parameters
+    ----------
+    df : pd.DataFrame
+        Dataframe over which the summarisation will be carried out
+    operation : str (for method) or function/method
+        Name of the function/method, e.g. could pass max or 'max'
+    cond : str
+        Condition that will be passed to the dataframe's .query method
+    col : str
+        Column used for the summary statistic
+    wrapper_type : str
+        One of: 'auto', 'method', 'function'.
+        For 'auto' the method will be used
+        unless it does not exist
+
+    Returns
+    -------
+    df_summary : pd.DataFrame
+        Dataframe of summary statistics
+        
+    """
+    
+    cond_method = lambda df, method, cond='tuple()', col=None: getattr(df.query(cond), method)() if col is None else getattr(df.query(cond)[col], method)()
+    cond_function = lambda df, function, cond='tuple()', col=None: function(df.query(cond)) if col is None else function(df.query(cond)[col])
+
+    if '__name__' in dir(operation) and wrapper_type!='function':
+        if operation.__name__ not in dir(df) and wrapper_type=='auto':
+            pass
+        else:
+            operation = operation.__name__
+        
+    wrapper_type_to_process = {
+        'auto': lambda df, operation, cond, col: cond_method(df, operation, cond, col) if operation in dir(df) else cond_function(df, operation, cond, col),
+        'method': cond_method,
+        'function': cond_function
     }
     
-    df_summarised = (df
-                     .apply(lambda df: pd.Series({
-                         name: func(df, **kwargs) 
-                         for name, (func, kwargs)
-                         in cleaned_agg_funcs.items()
-                     }))
-                    )
+    return wrapper_type_to_process[wrapper_type](df, operation, cond, col)
+
+def summarise(df:pd.DataFrame, agg_ops:dict):
+    """
+    summarise accepts a dictionary of aggregation operations that 
+    will be used to construct the returned summary dataframe. The
+    dictionary must map from the name of the new summary column to
+    either a function/method, or a tuple that contains a function/
+    method as well as a dictionary of keyword arguments. 
+
+    Parameters
+    ----------
+    df : pd.DataFrame
+        Dataframe over which the summarisation will be carried out
+    agg_ops : dict
+        Mapping from column names to aggregation operations
+
+    Returns
+    -------
+    df_summary : pd.DataFrame
+        Dataframe of summary statistics
+
+    e.g. 
+
+    ```
+    agg_ops = {
+        'max_customers': (max, dict(col='customers')),                   # By default the .max() method will be used but
+        'max_sales': (max, dict(col='sales', wrapper_type='function')),  # it can also be used as a function if specified.
+        'total_days': len
+        'days_open': (len, dict(cond='store_open == True')),
+        'sales_tues': ('sum', dict(col='sales', cond='day == 2'))        # When using methods they can be passed as a string
+    }
+
+    df2 = (df1
+           .groupby('store')
+           .pipe(summarise, agg_ops)
+          )
+    ```
+    """
     
-    return df_summarised
+    # Handling agg_ops with no kwargs
+    cleaned_agg_ops = {
+        k: (v if isinstance(v, Iterable) else (v, {}))
+        for k, v 
+        in agg_ops.items()
+    }
+    
+    # Calculating summary statistics
+    df_summary = (df
+                  .apply(lambda df: pd.Series({
+                      name: summarise_wrapper(df, operation, **kwargs) 
+                      for name, (operation, kwargs)
+                      in cleaned_agg_ops.items()
+                  }))
+                 )
+    
+    return df_summary
 
 reverse_dict = lambda original_dict: {
     value: key for key, value in original_dict.items()
