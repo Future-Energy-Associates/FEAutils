c5091d7f7af08dabf7c7fac03903c16b42a2cd37
ClemAtt <clement.attwood.18@ucl.ac.uk>
2021-01-16 14:29:01 +0000

    Adding functions to general and timeseries

--- FEAutils/general.py
+++ FEAutils/general.py
@@ -1,11 +1,13 @@
 import pandas as pd
 import requests
+import regex as re
 from IPython.display import JSON
 
 # General functions
 from collections.abc import Iterable
 
-def summarise_wrapper(df, operation, cond='tuple()', col=None, wrapper_type='auto'):
+
+def summarise_wrapper(df, operation, cond="tuple()", col=None, wrapper_type="auto"):
     """
     Carries out the individual summary operations 
 
@@ -30,25 +32,36 @@ def summarise_wrapper(df, operation, cond='tuple()', col=None, wrapper_type='aut
         Dataframe of summary statistics
         
     """
-    
-    cond_method = lambda df, method, cond='tuple()', col=None: getattr(df.query(cond), method)() if col is None else getattr(df.query(cond)[col], method)()
-    cond_function = lambda df, function, cond='tuple()', col=None: function(df.query(cond)) if col is None else function(df.query(cond)[col])
 
-    if '__name__' in dir(operation) and wrapper_type!='function':
-        if operation.__name__ not in dir(df) and wrapper_type=='auto':
+    cond_method = (
+        lambda df, method, cond="tuple()", col=None: getattr(df.query(cond), method)()
+        if col is None
+        else getattr(df.query(cond)[col], method)()
+    )
+    cond_function = (
+        lambda df, function, cond="tuple()", col=None: function(df.query(cond))
+        if col is None
+        else function(df.query(cond)[col])
+    )
+
+    if "__name__" in dir(operation) and wrapper_type != "function":
+        if operation.__name__ not in dir(df) and wrapper_type == "auto":
             pass
         else:
             operation = operation.__name__
-        
+
     wrapper_type_to_process = {
-        'auto': lambda df, operation, cond, col: cond_method(df, operation, cond, col) if operation in dir(df) else cond_function(df, operation, cond, col),
-        'method': cond_method,
-        'function': cond_function
+        "auto": lambda df, operation, cond, col: cond_method(df, operation, cond, col)
+        if operation in dir(df)
+        else cond_function(df, operation, cond, col),
+        "method": cond_method,
+        "function": cond_function,
     }
-    
+
     return wrapper_type_to_process[wrapper_type](df, operation, cond, col)
 
-def summarise(df_gb:pd.core.groupby.generic.DataFrameGroupBy, agg_ops:dict):
+
+def summarise(df_gb: pd.core.groupby.generic.DataFrameGroupBy, agg_ops: dict):
     """
     summarise accepts a dictionary of aggregation operations that 
     will be used to construct the returned summary dataframe. The
@@ -85,24 +98,24 @@ def summarise(df_gb:pd.core.groupby.generic.DataFrameGroupBy, agg_ops:dict):
           )
     ```
     """
-    
+
     # Handling agg_ops with no kwargs
     cleaned_agg_ops = {
-        k: (v if isinstance(v, Iterable) else (v, {}))
-        for k, v 
-        in agg_ops.items()
+        k: (v if isinstance(v, Iterable) else (v, {})) for k, v in agg_ops.items()
     }
-    
+
     # Calculating summary statistics
-    df_summary = (df_gb
-                  .apply(lambda df: pd.Series({
-                      name: summarise_wrapper(df, operation, **kwargs) 
-                      for name, (operation, kwargs)
-                      in cleaned_agg_ops.items()
-                  }))
-                 )
-    
+    df_summary = df_gb.apply(
+        lambda df: pd.Series(
+            {
+                name: summarise_wrapper(df, operation, **kwargs)
+                for name, (operation, kwargs) in cleaned_agg_ops.items()
+            }
+        )
+    )
+
     return df_summary
+
 
 reverse_dict = lambda original_dict: {
     value: key for key, value in original_dict.items()
@@ -131,3 +144,49 @@ def send_slack_msg(
         requests.post(slack_channel_url, headers=headers, data=data)
     except:
         print(f"Slack message failed to send\n{record_msg}")
+
+
+def camel_to_snake(camel, replacements=None):
+
+    """
+    Converts camel case to snake case. Accepts either a single string or a list.
+    Optionally provide replacement strings in a dictionary as {'original_string':'replacement_string'}
+    """
+
+    assert (
+        isinstance(camel, list)
+        or isinstance(camel, pd.core.indexes.base.Index)
+        or isinstance(camel, str)
+    ), "camel must be instance of str, list or pandas index"
+
+    if isinstance(camel, list):
+        camels = camel
+    elif isinstance(camel, pd.core.indexes.base.Index):
+        camels = list(camel)
+    elif isinstance(camel, str):
+        camels = [camel]
+
+    snakes = []
+
+    for cam in camels:
+
+        matches = re.finditer(
+            ".+?(?:(?<=[a-z])(?=[A-Z])|(?<=[A-Z])(?=[A-Z][a-z])|$)", cam
+        )
+
+        snake = "_".join([m.group(0).lower() for m in matches])
+
+        snake = (
+            snake.replace("-", "_")
+            .replace(" ", "_")
+            .replace("__", "_")
+            .replace("__", "_")
+        )
+
+        if replacements is not None:
+            for original, replacement in replacements.items():
+                snake = snake.replace(original, replacement)
+
+        snakes.append(snake)
+
+    return snakes if len(snakes) > 1 else snake
--- FEAutils/timeseries.py
+++ FEAutils/timeseries.py
@@ -1,6 +1,53 @@
-from datetime import datetime
+from datetime import datetime, timedelta
+import pandas as pd
 
 # converting UNIX to str and back
 str_format = "%d-%m-%y %H.%M.%S"
 unix_to_str = lambda unix: datetime.fromtimestamp(unix).strftime(str_format)
 str_to_unix = lambda string: int(datetime.strptime(string, str_format).timestamp())
+
+
+def create_delay_seconds(hour: int, minute: int):
+    """
+    Creates delay in seconds from now until specified hour and minute in the same 24 hour period.
+
+    Parameters
+    ----------
+    -hour:
+        Hour of the time to create delay for
+    -minute:
+        Minute of the time to create delay for
+
+    Returns
+    -------
+    -now:
+        The time now as a datetime object
+    -delay:
+        The delay until the specified times in seconds as an integer
+    -delay_time:
+        The time the delay is set to as a datetime object
+
+    """
+
+    now = datetime.now().replace(microsecond=0)
+    hours_delta = hour - now.hour if now.hour <= hour else hour + (24 - now.hour)
+
+    delay_time = now.replace(minute=minute, second=0, microsecond=0) + timedelta(
+        hours=hours_delta
+    )
+
+    delay = (delay_time - now).total_seconds()
+
+    return now, delay, delay_time
+
+
+def strfdelta(tdelta: timedelta, fmt: str):
+    """
+    Formats datetime timedelta object containing days, hours, minutes and seconds in a specfied string format.
+    """
+
+    d = {"days": tdelta.days}
+    d["hours"], rem = divmod(tdelta.seconds, 3600)
+    d["minutes"], d["seconds"] = divmod(rem, 60)
+
+    return fmt.format(**d)
